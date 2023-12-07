237cef5203c657f4f498590a9ef35aed6eb4336a
Kenneth McElvain <kenmcelvain@me.com>
2023-12-06 17:47:22 -0800

    To demo use of mu2e.py as a module

---
+++ v2/python/tst_mod.py
@@ -0,0 +1,12 @@
+#
+# Demonstrate that mu2e can be used as a module
+#
+import pathlib
+import yaml
+import mu2e_v2 as mu2e
+
+path = 'example.yaml'
+with open(path, "r") as f:
+    data = yaml.safe_load(f)
+    ndata = mu2e.processdata(data)
+    print("ndata = ", ndata)
